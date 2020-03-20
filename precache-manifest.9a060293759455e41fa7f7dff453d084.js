ac03143bcebb110b5fb8e11d27b8e079a02770cc
reham2 <rehamweb@gmail.com>
2020-03-20 18:49:33 +0200

    Updates

---
+++ precache-manifest.9a060293759455e41fa7f7dff453d084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c374354274035f389dd2",
+    "url": "/monstersapp/static/css/main.5e67a508.chunk.css"
+  },
+  {
+    "revision": "c374354274035f389dd2",
+    "url": "/monstersapp/static/js/main.c3743542.chunk.js"
+  },
+  {
+    "revision": "b7f6a85ab9f3db85c063",
+    "url": "/monstersapp/static/js/1.b7f6a85a.chunk.js"
+  },
+  {
+    "revision": "853cd9c0e7cf7fe61685",
+    "url": "/monstersapp/static/js/runtime~main.853cd9c0.js"
+  },
+  {
+    "revision": "cf05ad38329dc845daa82d48b6afe6db",
+    "url": "/monstersapp/index.html"
+  }
+];
